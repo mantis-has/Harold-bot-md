e5d50d2858e3104f1fb0804f27c03eb755c319ef
Dioneibi-rip <fandubdreams@gmail.com>
2025-04-16 22:42:59 -0400

    Actualizar run.bat

--- run.bat
+++ run.bat
@@ -1,17 +1,17 @@
 @echo off
-echo [ INFORMACION ] Yuki_Suou-Bot para Windows
+echo [ INFORMACION ] Ruby-Hoshino-Bot-MD para Windows
 
 REM Ejecutar el archivo setup.bat
-echo [Console @ Yuki_Suou] Verificando si los programas de ejecucion estan instalados...
+echo [Console @ Ruby_Hoshino] Verificando si los programas de ejecucion estan instalados...
 call setup.bat
 
 REM Verificar si setup.bat terminó correctamente
 if %errorlevel% neq 0 (
-    echo [Console @ Yuki_Suou] setup.bat termino con errores, pero se continuara con la ejecución.
+    echo [Console @ Ruby_Hoshino] setup.bat termino con errores, pero se continuara con la ejecución.
 )
 
 REM Ejecutar node index.js
-echo [Console @ Yuki_Suou] Iniciando Yuki_Suou-Bot...
+echo [Console @ Ruby_Hoshino] Iniciando Ruby-Hoshino-Bot-MD...
 node index.js
 if %errorlevel% neq 0 (
     echo [ERROR] Ocurrio un error. Verifica la instalacion.
