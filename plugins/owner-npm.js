6b628b1eb732a05708c6f21e08aaf8052967d8a5
nevi-dev <nevijose4@gmail.com>
2025-05-04 23:42:26 -0400

    Update owner-npm.js

--- plugins/owner-npm.js
+++ plugins/owner-npm.js
@@ -3,20 +3,20 @@ import { exec } from 'child_process';
 let handler = async (m, { conn }) => {
   m.reply(`${emoji2} instalando dependencias...`);
 
-  exec('npm install ytdl-core', (err, stdout, stderr) => {
+  exec('npm install ytdl-core sharp', (err, stdout, stderr) => {
     if (err) {
-      conn.reply(m.chat, `${msm} Error: No se pudo realizar la actualización.\nRazón: ${err.message}`, m);
+      conn.reply(m.chat, `${msm} log de la instalacion:.\nRazón: ${err.message}`, m);
       return;
     }
 
     if (stderr) {
-      console.warn('Advertencia durante la actualización:', stderr);
+      console.warn('Advertencia durante la instalacion de dependencias:', stderr);
     }
 
     if (stdout.includes('Already up to date.')) {
       conn.reply(m.chat, `${emoji4} El bot ya está actualizado.`, m);
     } else {
-      conn.reply(m.chat, `${emoji} Actualización realizada con éxito.\n\n${stdout}`, m);
+      conn.reply(m.chat, `${emoji} instalacion completa.\n\n${stdout}`, m);
     }
   });
 };
